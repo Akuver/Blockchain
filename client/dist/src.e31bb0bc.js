d2398242619ec135a561fc859a775b7b345b316a
Akuver <aanjaneykumar.verma@gmail.com>
2021-06-29 15:27:51 +0530

    .gitignore added

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -47360,7 +47360,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46255" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46773" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
